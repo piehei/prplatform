bca44615c6b09bd96a0715297334c8768992be64
Pietari Heino <ph@extreg.com>
2018-04-10 14:52:57 +0300

    add teachers field into course model

--- prplatform/courses/models.py
+++ prplatform/courses/models.py
@@ -1,9 +1,12 @@
 from django.urls import reverse
 from django.db import models
 
+from prplatform.users.models import User
+
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
+    teachers = models.ManyToManyField(User)
 
     def get_absolute_url(self):
         return reverse('courses:detail', kwargs={'pk': self.pk})
