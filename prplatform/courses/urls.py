14ca16bef870cf67143a6e69ba5a01329ac4f034
Pietari Heino <ph@extreg.com>
2018-04-10 13:54:39 +0300

    added possibility to create a new course

--- prplatform/courses/urls.py
+++ prplatform/courses/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "courses"
 urlpatterns = [
     url(regex=r"^$", view=views.CourseListView.as_view(), name="list"),
+    url(regex=r"create/$", view=views.CourseCreateView.as_view(), name="create"),
 ]
--- prplatform/courses/views.py
+++ prplatform/courses/views.py
@@ -1,6 +1,15 @@
-from django.views.generic import ListView
+from django.urls import reverse
+from django.views.generic import ListView, CreateView
 
 from .models import Course
+
+
+class CourseCreateView(CreateView):
+    model = Course
+    fields = ['name']
+
+    def get_success_url(self):
+        return reverse("users:detail", kwargs={"username": self.request.user.username})
 
 
 class CourseListView(ListView):
