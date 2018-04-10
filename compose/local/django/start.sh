68e897c2d06eebaecdab3170b5b36f7e86889689
Pietari Heino <ph@extreg.com>
2018-04-10 13:04:09 +0300

    modified local start.sh to loop and restart django server on errors

--- compose/local/django/start.sh
+++ compose/local/django/start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-set -o errexit
-set -o pipefail
+# set -o errexit
+# set -o pipefail
 set -o nounset
 set -o xtrace
 
-
 python manage.py migrate
-python manage.py runserver_plus 0.0.0.0:8000
+
+while true; do
+  echo "Restarting.........."
+  python manage.py runserver_plus 0.0.0.0:8000
+  sleep 2
+done
