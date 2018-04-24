1f32b4ff6011e5ef613043ff266b77deef86e52a
Pietari Heino <ph@extreg.com>
2018-04-24 15:47:27 +0300

    course urls.py now includes teacher area. update moved underneath it.

--- prplatform/courses/urls.py
+++ prplatform/courses/urls.py
@@ -7,8 +7,8 @@ app_name = "courses"
 urlpatterns = [
     url(regex=r"^$", view=views.CourseListView.as_view(), name="list"),
     path('<slug:base_url_slug>/<slug:url_slug>/', view=views.CourseDetailView.as_view(), name="detail"),
-    path('<slug:base_url_slug>/<slug:url_slug>/update', view=views.CourseUpdateView.as_view(), name="update"),
     path('<slug:base_url_slug>/<slug:url_slug>/enroll', view=views.CourseEnroll.as_view(), name="enroll"),
-    path('<slug:base_url_slug>/<slug:url_slug>/exercises/', include('prplatform.exercises.urls', namespace="exercises"))
-
+    path('<slug:base_url_slug>/<slug:url_slug>/exercises/', include('prplatform.exercises.urls', namespace="exercises")),
+    path('<slug:base_url_slug>/<slug:url_slug>/teacher', view=views.CourseTeacherView.as_view(), name="teacher"),
+    path('<slug:base_url_slug>/<slug:url_slug>/teacher/update', view=views.CourseUpdateView.as_view(), name="update"),
 ]
