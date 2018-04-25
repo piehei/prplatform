e8a5e263448521829d6d3fb83c47bedd461f002b
Pietari Heino <ph@extreg.com>
2018-04-25 11:35:56 +0300

    description and upload_instructions added to exercise

--- prplatform/exercises/models.py
+++ prplatform/exercises/models.py
@@ -5,9 +5,26 @@ from prplatform.core.models import TimeStampedModel
 from prplatform.users.models import User
 from prplatform.courses.models import Course
 
+
 class BaseExercise(TimeStampedModel):
+    """ This base exercise includes common fields for all exercises.
+        A single exercise is something where the student can:
+          * return his/her answer (teacher wants all to upload a pdf for instance)
+          * make a peer-review of someone else's answer
+
+        For each of the actual use-cases an inherited exercise shall be used.
+    """
+
     name = models.CharField("Name of the exercise", max_length=100)
     course = models.ForeignKey(Course, related_name='exercises', on_delete=models.CASCADE)
+    description = models.CharField(max_length=5000, blank=True)
+
 
 class FileUploadExercise(BaseExercise):
+    """ This is an exercise where the student uploads something to the system.
+        This uploaded file shall be the answer to this exercise.
+        This is not peer-reviewing but the thing that will be peer-reviewed.
+    """
+
     file = models.FileField(upload_to='uploads/', blank=True)
+    upload_instructions = models.CharField(max_length=500, blank=True)
