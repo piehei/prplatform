b53987275e52287fbc3285d27f52552c6a1f9985
Pietari Heino <ph@extreg.com>
2018-04-26 10:23:14 +0300

    teacher can now update exercises through courses/teacher/update

--- prplatform/courses/urls.py
+++ prplatform/courses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls import include, url
 
 from . import views
-from prplatform.exercises.views import ExerciseCreateView
+from prplatform.exercises.views import ExerciseCreateView, GeneralExerciseUpdateView
 
 app_name = "courses"
 urlpatterns = [
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<slug:base_url_slug>/<slug:url_slug>/teacher', view=views.CourseTeacherView.as_view(), name="teacher"),
     path('<slug:base_url_slug>/<slug:url_slug>/teacher/update', view=views.CourseUpdateView.as_view(), name="update"),
     path('<slug:base_url_slug>/<slug:url_slug>/teacher/create', view=ExerciseCreateView.as_view(), name="create-exercise"),
+    path('<slug:base_url_slug>/<slug:url_slug>/teacher/<int:pk>/update', view=GeneralExerciseUpdateView.as_view(), name="update-exercise"),
 ]
