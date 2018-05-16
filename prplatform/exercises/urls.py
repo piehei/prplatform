5248dbaf4da97336556e50063d8ab0e74c1ffdd5
Pietari Heino <ph@extreg.com>
2018-05-16 10:48:41 +0300

    exercise_list.html now points to submissions of each exercise

--- prplatform/exercises/urls.py
+++ prplatform/exercises/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 app_name = "exercises"
 urlpatterns = [
-    # path('', view=views.ExerciseListView.as_view(), name="exercise-list"),
     path('s/<int:pk>/', view=views.SubmissionExerciseDetailView.as_view(), name="submission-detail"),
     path('r/<int:pk>/', view=views.ReviewExerciseDetailView.as_view(), name="review-detail"),
+    path('s/<int:pk>/submissions/', include('prplatform.submissions.urls', namespace="submissions"))
 ]
