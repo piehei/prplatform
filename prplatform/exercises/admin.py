7de60c49098168cb0ce9e213e297e93ad04414ec
Pietari Heino <ph@extreg.com>
2018-05-17 11:57:46 +0300

    question and review exercise added to admin

--- prplatform/exercises/admin.py
+++ prplatform/exercises/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import SubmissionExercise
+from .models import SubmissionExercise, ReviewExercise, Question
 
 @admin.register(SubmissionExercise)
 class SubmissionExerciseModelAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(ReviewExercise)
+class ReviewExerciseModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionModelAdmin(admin.ModelAdmin):
+    pass
+
