5f971f20b791e6cf1fa54df9c2ddf8310b86526c
Pietari Heino <ph@extreg.com>
2018-06-04 13:13:20 +0300

    fixed UserManager inheritance -> from UserManager, not models.Manager

--- prplatform/users/models.py
+++ prplatform/users/models.py
@@ -1,17 +1,17 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
-class UserManager(models.Manager):
+class MyUserManager(UserManager):
     def get_by_natural_key(self, email):
         return self.get(email=email)
 
 
 class User(AbstractUser):
 
-    objects = UserManager()
+    objects = MyUserManager()
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
