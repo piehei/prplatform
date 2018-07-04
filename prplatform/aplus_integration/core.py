3a4f7de2872c040f53f9da2cabe48668aadda316
Pietari Heino <ph@extreg.com>
2018-07-04 10:35:04 +0300

    refactoring of aplus_integration core

--- prplatform/aplus_integration/core.py
+++ prplatform/aplus_integration/core.py
@@ -9,6 +9,9 @@ from django.core.files.temp import NamedTemporaryFile
 from prplatform.users.models import User
 from prplatform.submissions.models import OriginalSubmission
 
+import logging
+logger = logging.getLogger(__name__)
+
 # will be replaced with api key set for the course
 HEADERS = {'Authorization': 'Token xxx'}
 
@@ -20,14 +23,13 @@ if os.environ.get('REPLACE'):
     REPLACE = json.loads(os.environ.get('REPLACE'))
 
 
-def get_submissions_by_id(submission_exercise):
+def get_submissions(submission_exercise):
     """
         1. retrieve submissions to this exercise from aplus api
         2. if teacher has not set api key to the course, return err
         3. use only submissions whose submitter's emails are in REPLACE
         4. create orig submissions for each of them by calling create_submission
     """
-
     exercise_id = submission_exercise.aplus_exercise_id
 
     HEADERS['Authorization'] = f"Token {submission_exercise.course.aplus_apikey}"
@@ -35,21 +37,28 @@ def get_submissions_by_id(submission_exercise):
     results = cache.get(exercise_id)
     if not results:
         try:
-            resp = requests.get(f"https://plus.cs.tut.fi/api/v2/exercises/{exercise_id}/submissions/",
-                                headers=HEADERS)
+            results = []
 
-            results = resp.json()["results"]
+            url_next = f"https://plus.cs.tut.fi/api/v2/exercises/{exercise_id}/submissions/"
+            while url_next:
+                logger.info(url_next)
+                resp = requests.get(url_next, headers=HEADERS)
+                data = resp.json()
+                results = results + data["results"]
+                # this will be None when there's no additional pages of 100 results
+                url_next = data["next"]
+
         except Exception as e:
-            print(e)
-            print(resp.text)
-            return str(e)
+            logger.error(e)
+            logger.error8(e.text)
+            return False
 
-        cache.set(exercise_id, results, 60 * 5)
+        # cache.set(exercise_id, results, 60 * 5)
 
     users_and_ids = {}
     users_and_submissions = {}
 
-    print(f"length of results is {len(results)} --> getting them one by one")
+    logger.info(f"length of results is {len(results)} --> getting them one by one")
     for result in results:
 
         submission = cache.get(result['id'])
@@ -59,12 +68,13 @@ def get_submissions_by_id(submission_exercise):
 
         submitter_email = submission["submitters"][0]["email"]
 
-        if submitter_email not in REPLACE:
-            continue
+        # if submitter_email not in REPLACE:
+        #     continue
 
-        print(f"replacing: {submitter_email} -> {REPLACE[submitter_email]}")
-        submitter_email = REPLACE[submitter_email]
-        submission["submitters"][0]["email"] = submitter_email
+        if submitter_email in REPLACE:
+            logger.info(f"replacing: {submitter_email} -> {REPLACE[submitter_email]}")
+            submitter_email = REPLACE[submitter_email]
+            submission["submitters"][0]["email"] = submitter_email
 
         if submitter_email not in users_and_ids:
             users_and_ids[submitter_email] = []
@@ -73,10 +83,41 @@ def get_submissions_by_id(submission_exercise):
         if submitter_email not in users_and_submissions:
             users_and_submissions[submitter_email] = submission
 
-    print(users_and_ids)
+    logger.info("These will be added:")
+    logger.info(users_and_ids)
+
+    created_users = 0
+    created_submissions = 0
 
     for user in users_and_submissions:
-        create_submission(submission_exercise, users_and_submissions[user])
+        if create_user(users_and_submissions[user]):
+            created_users += 1
+
+        if create_submission(submission_exercise, users_and_submissions[user]):
+            created_submissions += 1
+
+    logger.info(f"Created {created_users} new users")
+    logger.info(f"Created {created_submissions} new submissions")
+
+    return True
+
+
+def create_user(aplus_submission):
+
+    email = aplus_submission["submitters"][0]["email"]
+    try:
+        user = User.objects.get(email=email)
+        return False
+
+    except Exception as e:
+        logger.info(e)
+        logger.info(f"USER WAS NOT FOUND BY EMAIL {email} --> creating a new one")
+
+        user = User.objects.create_user(email, email, '')
+        user.set_unusable_password()
+        user.is_active = False
+        user.save()
+        return True
 
 
 def create_submission(submission_exercise, aplus_submission):
@@ -87,23 +128,14 @@ def create_submission(submission_exercise, aplus_submission):
        4. create a new original submission with the file and submitter
     """
 
-    print("create submission")
-
-    email = aplus_submission["submitters"][0]["email"]
-    try:
-        user = User.objects.get(email=email)
-    except Exception as e:
-        print(e)
-        print(f"USER WAS NOT FOUND BY EMAIL {email}")
-
-        user = User.objects.create_user(email, email, '')
-        user.set_unusable_password()
-        user.is_active = False
-        user.save()
+    user = User.objects.get(email=aplus_submission["submitters"][0]["email"])
 
     if len(submission_exercise.submissions.filter(submitter=user)) > 0:
-        print("skipping this submission from aplus api -> the user already has one origsub")
-        return
+        # TODO: submissions arent replaced if new ones found --> should they?
+        #       one option would be to save the aplus submission id along with the submitted
+        #       file so that later on it could be compared and possibly replaced if needed
+        # logger.info("skipping this submission from aplus api -> the user already has one origsub")
+        return False
 
     file_url = aplus_submission["files"][0]["url"]
     file_blob = requests.get(file_url, headers=HEADERS)
@@ -119,5 +151,6 @@ def create_submission(submission_exercise, aplus_submission):
                         file=File(temp_file)
                         )
     new_orig_sub.save()
+    logger.info(new_orig_sub)
 
-    print(new_orig_sub)
+    return True
