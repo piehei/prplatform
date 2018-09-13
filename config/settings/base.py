316593420aab5cafa2f0d1c2afca118889716d9e
Pietari Heino <ph@extreg.com>
2018-09-13 14:09:09 +0300

    language_code => fi-FI for timestamps

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ DEBUG = env.bool('DJANGO_DEBUG', False)
 # In Windows, this must be set to your system time zone.
 TIME_ZONE = 'Europe/Helsinki'
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fi-FI'
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
