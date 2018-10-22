146467d034317a6861b83a8fb64aa218fd87fe94
Pietari Heino <ph@extreg.com>
2018-10-22 08:40:47 +0300

    ChooceForm nows shows submissions in order_by('name') order

--- prplatform/exercises/forms.py
+++ prplatform/exercises/forms.py
@@ -178,4 +178,10 @@ class ChooceForm(Form):
                      .exclude(
                         id__in=exercise.reviewable_exercise.submissions_by_submitter(user).values('id')
                      )
+
+        if exercise.reviewable_exercise.use_groups:
+            qs = qs.order_by('submitter_group__name')
+        else:
+            qs = qs.order_by('submitter_user__name')
+
         self.fields['choice'].queryset = qs
