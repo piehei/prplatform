c1208952576fbe2f3bfd24e784d32c4112f5e1f8
Pietari Heino <ph@extreg.com>
2018-11-17 13:10:53 +0200

    DeviationForm populates original deadline and excludes groups/users with existing deviations

--- prplatform/exercises/deviation_forms.py
+++ prplatform/exercises/deviation_forms.py
@@ -9,25 +9,33 @@ class DeviationForm(ModelForm):
     class Meta:
         model = SubmissionExerciseDeviation
         fields = ['user', 'group', 'new_deadline']
+        labels = {
+                'new_deadline': 'New deadline. The prepopulated value in the input field is the original deadline.'
+                }
         help_texts = {
                 'new_deadline': 'Date and time in format YYYY-MM-DD HH:MM, eg. 2018-09-12 23:59',
                 }
-        initial_data = {
-                'new_deadline': timezone.now()
-                 }
 
     def __init__(self, *args, **kwargs):
         self.exercise = kwargs.pop('exercise')
 
         super().__init__(*args, **kwargs)
 
+        self.fields['new_deadline'].initial = self.exercise.closing_time
+
         if self.exercise.use_groups:
             del self.fields['user']
-            self.fields['group'].queryset = self.exercise.course.student_groups.all()
+            self.fields['group'].queryset = self.exercise.course.student_groups \
+                                                .exclude(
+                                pk__in=self.exercise.deviations.values_list('group__pk', flat=True)
+                                                )
             self.fields['group'].required = True
         else:
             del self.fields['group']
-            self.fields['user'].queryset = self.exercise.course.students.all()
+            self.fields['user'].queryset = self.exercise.course.students \
+                                               .exclude(
+                                pk__in=self.exercise.deviations.values_list('user__pk', flat=True)
+                                                )
             self.fields['user'].required = True
 
     def clean(self):
