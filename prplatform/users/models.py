d2b9a5353f5305b5f5a531e42b53af844ba0ef0b
Pietari Heino <ph@extreg.com>
2018-11-19 11:14:16 +0200

    __str__ of User updated to NOT expose username

--- prplatform/users/models.py
+++ prplatform/users/models.py
@@ -19,7 +19,7 @@ class User(AbstractUser):
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
 
     def __str__(self):
-        return f"{self.username} {self.email}"
+        return f"{self.email}"
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
@@ -28,7 +28,10 @@ class User(AbstractUser):
 class StudentGroup(models.Model):
     name = models.CharField(max_length=30)
     course = models.ForeignKey('courses.Course', related_name='student_groups', on_delete=models.CASCADE)
+
+    # TODO: this field is not utilized anywhere currently
     students = models.ManyToManyField(User, related_name='my_groups')
+
     student_usernames = ArrayField(models.CharField(max_length=100))
 
     class Meta:
