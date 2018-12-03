d107887897def01407fbb2ac33b7eff1bc5331f5
Pietari Heino <ph@extreg.com>
2018-12-03 09:43:13 +0200

    ReviewSubmissions added to admin panel

--- prplatform/submissions/admin.py
+++ prplatform/submissions/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import OriginalSubmission
+from .models import OriginalSubmission, ReviewSubmission
 
 @admin.register(OriginalSubmission)
 class OriginalSubmissionModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ReviewSubmission)
+class ReviewSubmissionModelAdmin(admin.ModelAdmin):
     pass
