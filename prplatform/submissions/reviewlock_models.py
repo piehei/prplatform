3039456ba5e46be48eb9a2f074b2a8884d48d352
Pietari Heino <ph@extreg.com>
2018-12-17 11:05:32 +0200

    create reviewlock decides the target depending on the count of locks AND the count of reviews

--- prplatform/submissions/reviewlock_models.py
+++ prplatform/submissions/reviewlock_models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Count
 from django.core.exceptions import EmptyResultSet
 
 from .models import (
@@ -28,32 +29,35 @@ class ReviewLockManager(models.Manager):
             raise Exception("Group info doesn't match")
 
         if exercise.type == ReviewExercise.RANDOM:
-            print("type is RANDOM")
 
             """
-                TODO: pick original submission based on some other heuristics
-                      than just random value. something that has not received any
-                      reviews?
+                TODO: this algo does NOT take into account a situation where one updates their review
+                      which causes two reviews to be available for an original submission
             """
 
-            # this is all _last_ submissions by other submitters sorted by review count
+            # all original submissions that *could* be the target of the review
+            # annotated with their existing review and reviewlock counts
+            # the queryset is ordered in ascending order by reviews_and_locks sum and creation date
+            # ---> first item is the one with least reviews/locks and oldest creation date
             osub_candidates = OriginalSubmission.objects \
-                                           .filter(exercise=exercise.reviewable_exercise) \
-                                           .annotate(models.Count('reviewlocks')) \
-                                           .order_by('reviewlocks__count', 'created')
-
-            # osub_candidates are now ordered so that first item has least reviewlocks
-            # was created the longest time ago
-
-
+                                                .filter(exercise=exercise.reviewable_exercise) \
+                                                .annotate(
+                                                    reviews_and_locks=Count('reviewlocks', distinct=True)+Count('reviews', distinct=True)
+                                                    ) \
+                                                .order_by(
+                                                    'reviews_and_locks',
+                                                    'created'
+                                                    )
 
             # ATTENTION !!!!!!!!!!
             # the query just above *CANNOT* be joined with the order_by/distinct queries
             # that's why we need two queries
 
-            print(f"{osub_candidates.count()} -- before max_rev_per_sub exlude")
-            osub_candidates = osub_candidates.exclude(reviewlocks__count__gte=exercise.max_reviews_per_submission)
-            print(f"{osub_candidates.count()} -- after max_rev_per_sub exlude")
+            # print(f"{osub_candidates.count()} -- before max_rev_per_sub exlude")
+            # print(osub_candidates)
+            osub_candidates = osub_candidates.exclude(reviews_and_locks__gte=exercise.max_reviews_per_submission)
+            # print(f"{osub_candidates.count()} -- after max_rev_per_sub exlude")
+            # print(osub_candidates)
 
             if exercise.reviewable_exercise.use_groups:
                 latest_submission_ids = OriginalSubmission.objects.filter(exercise=exercise.reviewable_exercise) \
@@ -73,12 +77,15 @@ class ReviewLockManager(models.Manager):
                                                  .filter(id__in=latest_submission_ids)
 
             previous_revsub_ids = exercise.last_reviews_by(user).values('reviewed_submission__id')
-            print(f"previous_revsub_ids: {previous_revsub_ids}")
-            print(f"{osub_candidates.count()} -- before prev id exlude")
+            # print(f"previous_revsub_ids: {previous_revsub_ids}")
+            # print(f"{osub_candidates.count()} -- before prev id exlude")
+            # print(osub_candidates)
             osub_candidates = osub_candidates.exclude(pk__in=previous_revsub_ids)
-            print(f"{osub_candidates.count()} -- after prev id exlude")
+            # print(f"{osub_candidates.count()} -- after prev id exlude")
+            # print(osub_candidates)
             osub_candidates = osub_candidates.filter(state=OriginalSubmission.READY_FOR_REVIEW)
-            print(f"{osub_candidates.count()} -- after READY_FOR_REVIEW filter")
+            # print(f"{osub_candidates.count()} -- after READY_FOR_REVIEW filter")
+            # print(osub_candidates)
 
             if osub_candidates.count() == 0:
                 raise EmptyResultSet("nothing to review")
@@ -106,4 +113,7 @@ class ReviewLock(TimeStampedModel):
         if self.group:
             return f"{self.group}"
         return f"self.user"
+
+    def __str__(self):
+        return f"{self.user} {self.group} -- {self.original_submission}"
 
