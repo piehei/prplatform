0858739799c1672fc5d2fd26368cb0eeba25663a
Pietari Heino <ph@extreg.com>
2019-01-22 18:52:48 +0200

    local cron runner will use the new mgmt command for a-plus hook handling

--- crons/run.sh
+++ crons/run.sh
@@ -9,6 +9,6 @@ export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5
 echo "starting fetch"
 
 # this mgmt command fetches new submissions from APLUS api
-python /app/manage.py fetch
+python /app/manage.py retry_apicalls
 
 echo "fetch finished"
