095e01b9d3f545f694f44bae940cb9aeea946a07
Pietari Heino <ph@extreg.com>
2019-01-30 19:10:03 +0200

    ReviewLock search fields etc. improved in admin.py

--- prplatform/submissions/admin.py
+++ prplatform/submissions/admin.py
@@ -13,4 +13,8 @@ class ReviewSubmissionModelAdmin(admin.ModelAdmin):
 
 @admin.register(ReviewLock)
 class ReviewLockModelAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("course", "review_exercise", "created", "original_submission", "user", "group")
+    search_fields = ["review_exercise__name", "user__username", "user__email", "user__first_name", "user__last_name", "group__name"]
+
+    def course(self, obj):
+        return obj.review_exercise.course
