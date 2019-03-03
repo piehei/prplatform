dcbfa3c1e5b69f31db7ad29dab9b21c5274e09ba
Pietari Heino <ph@extreg.com>
2019-03-03 19:47:47 +0200

    Hide RE expiry hours if type not RANDOM on create/edit views

--- prplatform/static/js/reviewexercise.js
+++ prplatform/static/js/reviewexercise.js
@@ -1,65 +1,24 @@
 
 // THIS IS A TOTAL HACK
 // will be replaced with Vue.JS <3
-//
-//
-//
+// *another comment 10 months later: will it?*
 
 window.onload = function() {
 
+  // this will listen on changes of the Type option field
+  // and hide expiry hours field if the type changes to something
+  // else than RANDOM. RANDOM is the only type that supports
+  // expiry hours.
 
-  var PREFIX = "#id_form";
-
-  var TOTAL_FORMS = parseInt($(PREFIX + '-TOTAL_FORMS').val());
-  console.log("TOTAL_FORMS: ", TOTAL_FORMS);
-
-  var INITIAL_FORMS = parseInt($(PREFIX + '-INITIAL_FORMS').val());
-  console.log("INITIAL_FORMS: ", INITIAL_FORMS);
-
-  // these are all the questions in the formset
-  // there will be a total of 10 extra empty questions that might be taken into use
-  var multiFields = Array.prototype.slice.call(document.querySelectorAll('.multiField'));
-
-  // iterate over the fields and hide all *unused* fields from the view
-  multiFields.forEach(function(elem, index) {
-    console.log(elem, index)
-    if (index >= INITIAL_FORMS) {
-      elem.style.display = "none";
+  function handleChange(event) {
+    var expiry_hours = document.getElementById('div_id_reviewlock_expiry_hours');
+    if (event.target.value === 'RANDOM') {
+      expiry_hours.style.display = '';
+    } else {
+      expiry_hours.style.display = 'none';
     }
-  })
-
-  // if no questions has been added, show the first empty question
-  if (INITIAL_FORMS === 0) {
-    multiFields[0].style.display = "";
-    document.querySelector(PREFIX + '-0-ORDER').value = 1;
   }
 
-  $('#add-new-question').on('click', function() {
-    console.log('add-new-question clicked');
-
-    // when the add new question button is clicked
-    // find the first *unused* hidden question field and show it
-    for (var i = 0; i < multiFields.length; i += 1) {
-      if (multiFields[i].style.display === "none") {
-        multiFields[i].style.display = "";
-        document.querySelector(PREFIX + '-' + i + '-ORDER').value = i + 1;
-        break;
-      }
-    }
-
-  })
-
-  // when the form submit button is pressed
-  // go through all added questions and make sure their ORDER value is ""
-  // if text value is ""
-  // --> if this is not done backend thinks that a new question with order but WITHOUT text
-  //     was tried to be added
-  $('#create-review-exercise').on('click', function() {
-    for (var i = 0; i < multiFields.length; i += 1) {
-        var textInputField = document.querySelector(PREFIX + '-' + i + '-text');
-        if (textInputField.value === "") {
-          document.querySelector(PREFIX + '-' + i + '-ORDER').value = "";
-        }
-    }
-  })
+  document.getElementById('id_type')
+    .addEventListener('change', handleChange);
 }
