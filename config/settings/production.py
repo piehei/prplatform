8460f10841abfccff512b4d8ee1c1536431548bc
Pietari Heino <ph@extreg.com>
2019-04-24 18:00:53 +0300

    Disabling, cleanup of some unused features by cookiecutter

--- config/settings/production.py
+++ config/settings/production.py
@@ -65,27 +65,31 @@ X_FRAME_OPTIONS = 'DENY'
 # https://django-storages.readthedocs.io/en/latest/#installation
 INSTALLED_APPS += ['storages']  # noqa F405
 # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_AUTO_CREATE_BUCKET = True
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_QUERYSTRING_AUTH = False
-# DO NOT change these unless you know what you're doing.
-_AWS_EXPIRY = 60 * 60 * 24 * 7
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
-}
+
+#  by django cookie-cutter -> enable if you want
+#
+#  AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
+#  # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+#  AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
+#  # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+#  AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
+#  # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+#  AWS_AUTO_CREATE_BUCKET = True
+#  # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+#  AWS_QUERYSTRING_AUTH = False
+#  # DO NOT change these unless you know what you're doing.
+#  _AWS_EXPIRY = 60 * 60 * 24 * 7
+#  # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
+#  AWS_S3_OBJECT_PARAMETERS = {
+#      'CacheControl': f'max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate',
+#  }
 
 # STATIC
 # ------------------------
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # MEDIA
+# by django cookie-cutter -> enable if you want
 # ------------------------------------------------------------------------------
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # MEDIA_URL = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
