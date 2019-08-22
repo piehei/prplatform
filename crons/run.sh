c5d7ae7baf21235c25e16ad3dfc64f360b388a78
Pietari Heino <ph@extreg.com>
2019-08-22 19:18:25 +0300

    Use transaction.atomic() in retry_apicalls, cleanup

--- crons/run.sh
+++ crons/run.sh
@@ -6,9 +6,6 @@ source /app/.envs/.local/.postgres
 
 export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_DB}"
 
-echo "starting fetch"
-
-# this mgmt command fetches new submissions from APLUS api
+echo "starting retry_apicalls"
 python /app/manage.py retry_apicalls
-
 echo "fetch finished"
--- prplatform/aplus_integration/management/commands/retry_apicalls.py
+++ prplatform/aplus_integration/management/commands/retry_apicalls.py
@@ -1,14 +1,11 @@
 
 from django.core.management.base import BaseCommand
-
-from prplatform.exercises.models import SubmissionExercise
+from django.db import transaction
 
 from prplatform.aplus_integration.models import AplusAPICallRequest
 from prplatform.aplus_integration.core import (
         handle_submission_by_hook,
     )
-
-import requests
 
 import logging
 logger = logging.getLogger(__name__)
@@ -27,9 +24,18 @@ class Command(BaseCommand):
         logger.info(f"Found {pending_calls.count()} AplusAPICallRequests ---> fetch now")
 
         for call in pending_calls:
-            can_delete, msg = handle_submission_by_hook(call)
-            if can_delete:
-                logger.info('hook request handled -> deleting')
-                call.delete()
+            with transaction.atomic():
+                # PLEASE NOTE:
+                # this transaction *only* means rollback, the possible exception
+                # is not handled here in any way which will terminate the mgmt command
+                # call. since the system is configured to use sentry reporting, the
+                # error will be reported there and sufficient means may be taken then.
+                # if there's one tricky submission causing problems, it may be removed
+                # through django admin or shell access.
+                logger.debug(f"Starting to handle {call}")
+                can_delete, msg = handle_submission_by_hook(call)
+                if can_delete:
+                    logger.debug('Hook handled the call -> deleting')
+                    call.delete()
 
-
+        logger.info(f"Handled all AplusAPICallRequests")
